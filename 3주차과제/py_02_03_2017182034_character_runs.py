676c248bcfa8e7bccb9a5ed4c83ef1598fae8049
limboboat <50803101+limboboat@users.noreply.github.com>
2020-09-19 00:15:23 +0900

    누락된 파일....ㅜ

--- 3주차과제/py_02_03_2017182034_character_runs.py
+++ 3주차과제/py_02_03_2017182034_character_runs.py
@@ -1,1 +1,26 @@
-from pico import *
+from pico2d import *
+
+open_canvas()
+grass = load_image('C:/Users/msi/2d_project/3주차과제/grass.png')
+character = load_image('C:/Users/msi/2d_project/3주차과제/animation_sheet.png')
+
+x=0
+frame = 0
+action=0
+while(x<800):
+    clear_canvas_now()
+    grass.draw(400,30)
+    character.clip_draw(frame * 100, 100*action, 100, 100, x, 85)
+    update_canvas()
+    get_events()
+
+    x += 2
+    if x%100 ==0:
+        action=(action+1)%4
+
+    frame = (frame + 1) % 8
+
+    delay(0.005)
+
+
+close_canvas()
