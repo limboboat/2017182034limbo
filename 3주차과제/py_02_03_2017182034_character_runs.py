efa3f11efa94c663afb8fb49970ed755f120f55f
limboboat <50803101+limboboat@users.noreply.github.com>
2020-09-20 21:38:26 +0900

    수정사항

--- 3주차과제/py_02_03_2017182034_character_runs.py
+++ 3주차과제/py_02_03_2017182034_character_runs.py
@@ -7,20 +7,19 @@ character = load_image('C:/Users/msi/2d_project/3주차과제/animation_sheet.pn
 x=0
 frame = 0
 action=0
+
 while(x<800):
-    clear_canvas_now()
-    grass.draw(400,30)
+    clear_canvas()
+    grass.draw(400, 30)
     character.clip_draw(frame * 100, 100*action, 100, 100, x, 85)
     update_canvas()
-    get_events()
-
-    x += 2
+    x += 5
     if x%100 ==0:
         action=(action+1)%4
 
     frame = (frame + 1) % 8
 
-    delay(0.005)
-
+    #delay(0.005)
+    get_events()
 
 close_canvas()
